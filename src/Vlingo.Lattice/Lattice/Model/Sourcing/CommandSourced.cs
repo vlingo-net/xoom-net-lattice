9b8c059b14e9ab8514fdf0531dc55cb73b8232ae
Tomasz Jaskula <thomasv1000@hotmail.fr>
2020-01-25 21:12:52 +0100

    Add CommandSourced

---
+++ src/Vlingo.Lattice/Lattice/Model/Sourcing/CommandSourced.cs
@@ -0,0 +1,19 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+namespace Vlingo.Lattice.Model.Sourcing
+{
+    /// <summary>
+    /// A <see cref="Sourced{T}"/> for concrete types of <see cref="Command"/>.
+    /// </summary>
+    public abstract class CommandSourced : Sourced<DomainEvent>
+    {
+        public CommandSourced(string streamName) : base(streamName)
+        {
+        }
+    }
+}
