d216d85d64c56126eeb2823c4fc092b63ff3f09f
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-01-04 00:05:59 +0100

    Add ApplyFailedException and Applicable

---
+++ src/Vlingo.Lattice/Lattice/Model/Applicable.cs
@@ -0,0 +1,36 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System.Collections.Generic;
+using Vlingo.Actors;
+using Vlingo.Symbio;
+
+namespace Vlingo.Lattice.Lattice.Model
+{
+    /// <summary>
+    /// The elements used in the attempted <code>Apply()</code>
+    /// </summary>
+    /// <typeparam name="T">The type of the state</typeparam>
+    public class Applicable<T>
+    {
+        public Applicable(T state, IEnumerable<Source> sources, Metadata metadata, CompletionSupplier<T> completionSupplier)
+        {
+            State = state;
+            Sources = sources;
+            Metadata = metadata;
+            CompletionSupplier = completionSupplier;
+        }
+        
+        public CompletionSupplier<T> CompletionSupplier { get; }
+        
+        public Metadata Metadata { get; }
+        
+        public IEnumerable<Source> Sources { get; }
+        
+        public T State { get; }
+    }
+}
---
+++ src/Vlingo.Lattice/Lattice/Model/ApplyFailedException.cs
@@ -0,0 +1,28 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using Vlingo.Symbio;
+
+namespace Vlingo.Lattice.Lattice.Model
+{
+    /// <summary>
+    /// An Exception used to indicate the failure of an attempt to <code>Apply()</code>
+    /// state and/or <see cref="Source"/> instances.
+    /// </summary>
+    /// <typeparam name="T">The type of the state</typeparam>
+    public class ApplyFailedException<T> : Exception
+    {
+        public ApplyFailedException(Applicable<T> applicable) => Applicable = applicable;
+
+        public ApplyFailedException(Applicable<T> applicable, string? message) : base(message) => Applicable = applicable;
+
+        public ApplyFailedException(Applicable<T> applicable, string? message, Exception? innerException) : base(message, innerException) => Applicable = applicable;
+
+        public Applicable<T> Applicable { get; }
+    }
+}
