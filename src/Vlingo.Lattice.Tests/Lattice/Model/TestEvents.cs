0ede9ad2573fb892495538c9419881219b827380
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-01-04 08:19:38 +0100

    Add TestEvents

---
+++ src/Vlingo.Lattice.Tests/Lattice/Model/TestEvents.cs
@@ -0,0 +1,50 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using Vlingo.Symbio;
+
+namespace Vlingo.Lattice.Tests.Lattice.Model
+{
+    public static class TestEvents
+    {
+        public abstract class Event : Source<Event>
+        {
+            public static Event1 Event1 => new Event1();
+            
+            public static Event2 Event2 => new Event2();
+            
+            public static Event3 Event3 => new Event3();
+            
+            public override bool Equals(object obj)
+            {
+                if (obj == null || obj.GetType() != GetType())
+                {
+                    return false;
+                }
+                
+                return true;
+            }
+
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
+        }
+        
+        public class Event1 : Event
+        {
+        }
+        
+        public class Event2 : Event
+        {
+        }
+        
+        public class Event3 : Event
+        {
+        }
+    }
+}
