0e54c10a059e25ec9f7c3363c32978049d617f3e
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-01-04 08:32:21 +0100

    Provide basic model

---
+++ src/Vlingo.Lattice/Lattice/Model/Command.cs
@@ -0,0 +1,32 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using Vlingo.Symbio;
+
+namespace Vlingo.Lattice.Lattice.Model
+{
+    /// <summary>
+    /// A abstract base for commands, which are considered a type of <see cref="Source{T}"/>.
+    /// </summary>
+    public abstract class Command : Source<Command>
+    {
+        /// <summary>
+        /// Construct my default state with a type version of 1.
+        /// </summary>
+        protected Command()
+        {
+        }
+        
+        /// <summary>
+        /// Construct my default state with a <paramref name="commandTypeVersion"/> greater than 1.
+        /// </summary>
+        /// <param name="commandTypeVersion">The int version of this command type</param>
+        protected Command(int commandTypeVersion) : base(commandTypeVersion)
+        {
+        }
+    }
+}
---
+++ src/Vlingo.Lattice/Lattice/Model/DomainEvent.cs
@@ -0,0 +1,32 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using Vlingo.Symbio;
+
+namespace Vlingo.Lattice.Lattice.Model
+{
+    /// <summary>
+    /// A abstract base for events, which are considered a type of <see cref="Source{T}"/>.
+    /// </summary>
+    public abstract class DomainEvent : Source<DomainEvent>
+    {
+        /// <summary>
+        /// Construct my default state with a type version of 1.
+        /// </summary>
+        protected DomainEvent()
+        {
+        }
+        
+        /// <summary>
+        /// Construct my default state with a <paramref name="eventTypeVersion"/> greater than 1.
+        /// </summary>
+        /// <param name="eventTypeVersion">The int version of this command type</param>
+        protected DomainEvent(int eventTypeVersion) : base(eventTypeVersion)
+        {
+        }
+    }
+}
