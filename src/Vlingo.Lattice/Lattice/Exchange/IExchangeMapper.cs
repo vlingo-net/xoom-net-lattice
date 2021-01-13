b0e8eda3ebb16e803041e33a4079ff35f0ad2f0f
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-01-13 20:52:59 +0100

    Adding IExchangeMapper

--- src/Vlingo.Lattice/Lattice/Exchange/IExchangeMapper.cs
+++ src/Vlingo.Lattice/Lattice/Exchange/IExchangeMapper.cs
@@ -7,8 +7,25 @@
 
 namespace Vlingo.Lattice.Exchange
 {
-    public interface IExchangeMapper
+    /// <summary>
+    /// Supports mapping a local type to external type, and a external type to local type.
+    /// </summary>
+    /// <typeparam name="TLocal">The local type</typeparam>
+    /// <typeparam name="TExternal">The external type</typeparam>
+    public interface IExchangeMapper<TLocal, TExternal>
     {
+        /// <summary>
+        /// Gets the external type given the local type.
+        /// </summary>
+        /// <param name="local">The <typeparamref name="TLocal"/> local type to map</param>
+        /// <returns>The <typeparamref name="TExternal"/> typed message</returns>
+        TExternal LocalToExternal(TLocal local);
         
+        /// <summary>
+        /// Gets the local type given the external type.
+        /// </summary>
+        /// <param name="external">The <typeparamref name="TExternal"/> external type to map</param>
+        /// <returns>The <typeparamref name="TLocal"/> typed message</returns>
+        TLocal ExternalToLocal(TExternal external);
     }
 }
