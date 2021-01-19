f12e40c871b414af66d89ff6af6c9f10822825e0
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-01-19 20:55:48 +0100

    Adding Info

--- src/Vlingo.Lattice/Lattice/Model/Process/Info.cs
+++ src/Vlingo.Lattice/Lattice/Model/Process/Info.cs
@@ -5,10 +5,39 @@
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
 
+using System;
+using Vlingo.Lattice.Exchange;
+
 namespace Vlingo.Lattice.Model.Process
 {
-    public class Info
+    /// <summary>
+    /// Holder of registration information.
+    /// </summary>
+    /// <typeparam name="T">The type of Process of the registration</typeparam>
+    public abstract class Info<T>
     {
-        
+        public IExchange Exchange { get; }
+        public string ProcessName { get; }
+
+        public Type ProcessType => typeof(T);
+
+        /// <summary>
+        /// Construct my default state.
+        /// </summary>
+        /// <param name="processName">The string name of the Process</param>
+        /// <param name="exchange">The exchange</param>
+        public Info(string processName, IExchange exchange)
+        {
+            ProcessName = processName;
+            Exchange = exchange;
+        }
+
+        /// <summary>
+        /// Construct my default state.
+        /// </summary>
+        /// <param name="processName">The string name of the Process</param>
+        public Info(string processName) : this(processName, NullExchange.Instance)
+        {
+        }
     }
 }
