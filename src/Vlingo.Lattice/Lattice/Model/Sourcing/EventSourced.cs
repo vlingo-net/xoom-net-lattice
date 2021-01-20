e6e685455b763386789e3ede3839f79290b1c1a7
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-01-20 23:23:40 +0100

    Add EventSourced.cs

---
+++ src/Vlingo.Lattice/Lattice/Model/Sourcing/EventSourced.cs
@@ -0,0 +1,19 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+namespace Vlingo.Lattice.Model.Sourcing
+{
+    /// <summary>
+    /// A <see cref="Sourced{T}"/> for concrete types of <see cref="DomainEvent"/>.
+    /// </summary>
+    public abstract class EventSourced : Sourced<DomainEvent>
+    {
+        public EventSourced(string streamName) : base(streamName)
+        {
+        }
+    }
+}
