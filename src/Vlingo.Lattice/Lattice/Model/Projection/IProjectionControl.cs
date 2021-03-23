fc61bfea12ac94f9c73dc849fe9bdc172fb8afa0
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-03-24 00:49:53 +0100

    Add IProjectionControl

--- src/Vlingo.Lattice/Lattice/Model/Projection/IProjectionControl.cs
+++ src/Vlingo.Lattice/Lattice/Model/Projection/IProjectionControl.cs
@@ -5,10 +5,34 @@
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
 
+using System;
+
 namespace Vlingo.Lattice.Model.Projection
 {
+    /// <summary>
+    /// Defines the control management for a given <see cref="IProjection"/>.
+    /// </summary>
     public interface IProjectionControl
     {
+        /// <summary>
+        /// Gets the <see cref="Confirmer"/> for the given <see cref="IProjectable"/> through
+        /// which confirmation can be performed as a single operation.
+        /// </summary>
+        /// <param name="projectable">The <see cref="IProjectable"/> requiring confirmation of completed projections operations</param>
+        /// <param name="control">The <see cref="IProjectionControl"/></param>
+        /// <returns>The <see cref="Confirmer"/></returns>
+        Action ConfirmerFor(IProjectable projectable, IProjectionControl control);
         
+        /// <summary>
+        /// Confirms that all projection operations have been completed.
+        /// </summary>
+        /// <param name="projectionId">the string unique identity of the projection operation.</param>
+        void ConfirmProjected(string projectionId);
+    }
+
+    public static class ProjectionControlExtensions
+    {
+        public static Confirmer ConfirmerFor(this IProjectionControl projectionControl, IProjectable projectable, IProjectionControl control) 
+            => new Confirmer(() => control.ConfirmProjected(projectable.ProjectionId));
     }
 }
