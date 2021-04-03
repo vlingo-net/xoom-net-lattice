c5f546dd1038dc2be3b9c97f2e28f3c9d595d049
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-04-03 23:17:37 +0200

    Implement JournalProjectionDispatcherTest

--- src/Vlingo.Lattice.Tests/Lattice/Model/Projection/JournalProjectionDispatcherTest.cs
+++ src/Vlingo.Lattice.Tests/Lattice/Model/Projection/JournalProjectionDispatcherTest.cs
@@ -23,28 +23,63 @@ using Xunit.Abstractions;
 
 namespace Vlingo.Tests.Lattice.Model.Projection
 {
-    public class JournalProjectionDispatcherTest
+    public class JournalProjectionDispatcherTest : IDisposable
     {
         private static string AccessJournal = "journal";
         private static string AccessProjection = "projection";
         private static string StreamName = "A123";
         
-        private AccessHolder accessHolder;
-        private IAppendResultInterest appendInterest;
-        private IDispatcher<Dispatchable<IEntry, IState>> dispatcher;
-        private IJournal<string> journal;
-        private World world;
+        private AccessHolder _accessHolder;
+        private readonly IAppendResultInterest _appendInterest;
+        private readonly IJournal<string> _journal;
+        private readonly World _world;
+        
+        [Fact]
+        public void TestThatOneTwoAllEventsProject()
+        {
+            _accessHolder.AccessJournalFor(1);
+            _accessHolder.AccessProjectionFor(5); // One, Two, and All
+
+            _journal.AppendAll<ISource>(StreamName, 1, new List<ISource> {new OneHappened(), new TwoHappened(), new ThreeHappened()}, _appendInterest, _accessHolder);
+
+            Assert.Equal(5, _accessHolder.AccessProjection.ReadFrom<int>(AccessProjection));
+            Assert.Equal(1, _accessHolder.AccessJournal.ReadFrom<int>(AccessJournal));
+        }
+        
+        [Fact]
+        public void TestThatOneEventProject()
+        {
+            _accessHolder.AccessJournalFor(1);
+            _accessHolder.AccessProjectionFor(2); // One and All
+
+            _journal.Append(StreamName, 1, new OneHappened(), _appendInterest, _accessHolder);
+
+            Assert.Equal(1, _accessHolder.AccessJournal.ReadFrom<int>(AccessJournal));
+            Assert.Equal(2, _accessHolder.AccessProjection.ReadFrom<int>(AccessProjection));
+        }
+        
+        [Fact]
+        public void TestThatTwoEventProject()
+        {
+            _accessHolder.AccessJournalFor(1);
+            _accessHolder.AccessProjectionFor(2); // Two and All
+
+            _journal.Append(StreamName, 1, new TwoHappened(), _appendInterest, _accessHolder);
+
+            Assert.Equal(1, _accessHolder.AccessJournal.ReadFrom<int>(AccessJournal));
+            Assert.Equal(2, _accessHolder.AccessProjection.ReadFrom<int>(AccessProjection));
+        }
         
         [Fact]
         public void TestThatThreeEventProject()
         {
-            accessHolder.AccessJournalFor(1);
-            accessHolder.AccessProjectionFor(1); // Only All
+            _accessHolder.AccessJournalFor(1);
+            _accessHolder.AccessProjectionFor(1); // Only All
 
-            journal.Append(StreamName, 1, new ThreeHappened(), appendInterest, accessHolder);
+            _journal.Append(StreamName, 1, new ThreeHappened(), _appendInterest, _accessHolder);
 
-            Assert.Equal(1, accessHolder.AccessJournal.ReadFrom<int>(AccessJournal));
-            Assert.Equal(1, accessHolder.AccessProjection.ReadFrom<int>(AccessProjection));
+            Assert.Equal(1, _accessHolder.AccessJournal.ReadFrom<int>(AccessJournal));
+            Assert.Equal(1, _accessHolder.AccessProjection.ReadFrom<int>(AccessProjection));
         }
 
         public JournalProjectionDispatcherTest(ITestOutputHelper output)
@@ -52,38 +87,38 @@ namespace Vlingo.Tests.Lattice.Model.Projection
             var converter = new Converter(output);
             Console.SetOut(converter);
             
-            world = World.StartWithDefaults("test-journal-projections");
+            _world = World.StartWithDefaults("test-journal-projections");
 
-            accessHolder = new AccessHolder();
+            _accessHolder = new AccessHolder();
 
             var descriptions = new List<ProjectToDescription>
             {
                 ProjectToDescription<OneHappenedProjectionActor>.With<OneHappened>(() =>
-                    new OneHappenedProjectionActor(accessHolder)),
+                    new OneHappenedProjectionActor(_accessHolder)),
                 ProjectToDescription<TwoHappenedProjectionActor>.With<TwoHappened>(() =>
-                    new TwoHappenedProjectionActor(accessHolder)),
+                    new TwoHappenedProjectionActor(_accessHolder)),
                 ProjectToDescription<AllHappenedProjectionActor>
-                    .With<OneHappened>(() => new AllHappenedProjectionActor(accessHolder))
+                    .With<OneHappened>(() => new AllHappenedProjectionActor(_accessHolder))
                     .AndWith<TwoHappened>()
                     .AndWith<ThreeHappened>()
             };
 
             var dispatcherProtocols =
-                world.Stage.ActorFor(
+                _world.Stage.ActorFor(
                     new Type[] { typeof(IDispatcher<Dispatchable<IEntry, IState>>), typeof(IProjectionDispatcher) },
                     Definition.Has(() => new TextProjectionDispatcherActor(descriptions)));
 
             var dispatchers = Protocols.Two<IDispatcher<Dispatchable<IEntry, IState>>, IProjectionDispatcher>(dispatcherProtocols);
 
-            dispatcher = dispatchers._1;
+            var dispatcher = dispatchers._1;
 
-            journal = Journal<string>.Using<InMemoryJournalActor<string>>(world.Stage, dispatcher);
+            _journal = Journal<string>.Using<InMemoryJournalActor<string>>(_world.Stage, dispatcher);
 
-            EntryAdapterProvider.Instance(world).RegisterAdapter(new OneHappenedAdapter());
-            EntryAdapterProvider.Instance(world).RegisterAdapter(new TwoHappenedAdapter());
-            EntryAdapterProvider.Instance(world).RegisterAdapter(new ThreeHappenedAdapter());
+            EntryAdapterProvider.Instance(_world).RegisterAdapter(new OneHappenedAdapter());
+            EntryAdapterProvider.Instance(_world).RegisterAdapter(new TwoHappenedAdapter());
+            EntryAdapterProvider.Instance(_world).RegisterAdapter(new ThreeHappenedAdapter());
 
-            appendInterest = world.Stage.ActorFor<IAppendResultInterest>(() => new JournalAppendResultInterest());
+            _appendInterest = _world.Stage.ActorFor<IAppendResultInterest>(() => new JournalAppendResultInterest());
         }
         
         public class AccessHolder
@@ -94,24 +129,20 @@ namespace Vlingo.Tests.Lattice.Model.Projection
             public AccessSafely AccessJournal { get; private set; }
             public AccessSafely AccessProjection { get; private set; }
 
-            public AccessSafely AccessJournalFor(int times)
+            public void AccessJournalFor(int times)
             {
                 AccessJournal = AccessSafely.AfterCompleting(times);
 
                 AccessJournal.WritingWith<int>(JournalProjectionDispatcherTest.AccessJournal, x => _accessJournalCount.IncrementAndGet());
                 AccessJournal.ReadingWith(JournalProjectionDispatcherTest.AccessJournal, () => _accessJournalCount.Get());
-
-                return AccessJournal;
             }
 
-            public AccessSafely AccessProjectionFor(int times)
+            public void AccessProjectionFor(int times)
             {
                 AccessProjection = AccessSafely.AfterCompleting(times);
 
                 AccessProjection.WritingWith<int>(JournalProjectionDispatcherTest.AccessProjection, x => _accessProjectionCount.IncrementAndGet());
                 AccessProjection.ReadingWith(JournalProjectionDispatcherTest.AccessProjection, () => _accessProjectionCount.Get());
-
-                return AccessProjection;
             }
         }
         
@@ -130,13 +161,11 @@ namespace Vlingo.Tests.Lattice.Model.Projection
         public class OneHappenedProjectionActor : Actor, IProjection
         {
             private readonly AccessHolder _accessHolder;
-            private int _count;
 
             public OneHappenedProjectionActor(AccessHolder accessHolder) => _accessHolder = accessHolder;
 
             public void ProjectWith(IProjectable projectable, IProjectionControl control)
             {
-                _count = 0;
                 foreach (var entry in projectable.Entries)
                 {
                     switch (entry.Typed.Name)
@@ -155,13 +184,11 @@ namespace Vlingo.Tests.Lattice.Model.Projection
         public class TwoHappenedProjectionActor : Actor, IProjection
         {
             private readonly AccessHolder _accessHolder;
-            private int _count;
 
             public TwoHappenedProjectionActor(AccessHolder accessHolder) => _accessHolder = accessHolder;
 
             public void ProjectWith(IProjectable projectable, IProjectionControl control)
             {
-                _count = 0;
                 foreach (var entry in projectable.Entries)
                 {
                     switch (entry.Typed.Name)
@@ -208,28 +235,28 @@ namespace Vlingo.Tests.Lattice.Model.Projection
             public void AppendResultedIn<TSource, TSnapshotState>(IOutcome<StorageException, Result> outcome, string streamName, int streamVersion, TSource source,
                 Optional<TSnapshotState> snapshot, object @object) where TSource : ISource
             {
-                Logger.Debug("APPENDED");
+                Logger.Debug("APPENDED Source");
                 ((AccessHolder) @object).AccessJournal.WriteUsing(AccessJournal, 1);
             }
 
             public void AppendResultedIn<TSource, TSnapshotState>(IOutcome<StorageException, Result> outcome, string streamName, int streamVersion, TSource source,
                 Metadata metadata, Optional<TSnapshotState> snapshot, object @object) where TSource : ISource
             {
-                Logger.Debug("APPENDED");
+                Logger.Debug("APPENDED Source");
                 ((AccessHolder) @object).AccessJournal.WriteUsing(AccessJournal, 1);
             }
 
             public void AppendAllResultedIn<TSource, TSnapshotState>(IOutcome<StorageException, Result> outcome, string streamName, int streamVersion,
                 IEnumerable<TSource> sources, Optional<TSnapshotState> snapshot, object @object) where TSource : ISource
             {
-                Logger.Debug("APPENDED");
+                Logger.Debug("APPENDED Sources");
                 ((AccessHolder) @object).AccessJournal.WriteUsing(AccessJournal, 1);
             }
 
             public void AppendAllResultedIn<TSource, TSnapshotState>(IOutcome<StorageException, Result> outcome, string streamName, int streamVersion,
                 IEnumerable<TSource> sources, Metadata metadata, Optional<TSnapshotState> snapshot, object @object) where TSource : ISource
             {
-                Logger.Debug("APPENDED");
+                Logger.Debug("APPENDED Sources");
                 ((AccessHolder) @object).AccessJournal.WriteUsing(AccessJournal, 1);
             }
         }
@@ -301,6 +328,12 @@ namespace Vlingo.Tests.Lattice.Model.Projection
                 var serialization = JsonSerialization.Serialized(source);
                 return new TextEntry(id, typeof(ThreeHappened), version, serialization, metadata);
             }
+        }
+
+        public void Dispose()
+        {
+            _accessHolder = null;
+            _world.Terminate();
         }
     }
 }
