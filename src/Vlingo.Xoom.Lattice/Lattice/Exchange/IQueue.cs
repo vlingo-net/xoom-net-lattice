744264ca2d3c24d145f6fa0166ac8cd5995b7643
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-05-13 21:49:22 +0200

    Implement IQueue

---
+++ src/Vlingo.Xoom.Lattice/Lattice/Exchange/IQueue.cs
@@ -0,0 +1,17 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+namespace Vlingo.Xoom.Lattice.Exchange
+{
+    /// <summary>
+    /// Defines a message exchange, as a queue, through which any number of related
+    /// <see cref="IExchangeSender{T}"/>, <see cref="IExchangeReceiver{T}"/>, and <see cref="IExchangeAdapter{TLocal,TExternal,TExchange}"/> components
+    /// </summary>
+    public interface IQueue : IExchange
+    {
+    }
+}
