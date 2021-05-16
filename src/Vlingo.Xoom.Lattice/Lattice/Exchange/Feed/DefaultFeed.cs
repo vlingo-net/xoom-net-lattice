aec6a42baffb8ddeea52f08180fd673c951c9ac6
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-05-16 15:58:02 +0200

    Implement Feed and DefaultFeed

---
+++ src/Vlingo.Xoom.Lattice/Lattice/Exchange/Feed/DefaultFeed.cs
@@ -0,0 +1,49 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using Vlingo.Xoom.Actors;
+using Vlingo.Xoom.Symbio;
+using Vlingo.Xoom.Symbio.Store;
+
+namespace Vlingo.Xoom.Lattice.Exchange.Feed
+{
+    /// <summary>
+    /// The default implementation of <see cref="Feed{T}"/>. See the default behaviors
+    /// in <see cref="Feed{T}"/> for specific defaults.
+    /// </summary>
+    public class DefaultFeed : Feed<TextEntry>
+    {
+        private readonly string _exchangeName;
+        private readonly Type _feederType;
+        private readonly IEntryReader<TextEntry> _entryReaderType;
+        private readonly IFeeder _feeder;
+
+        public override IEntryReader<TextEntry> EntryReaderType => _entryReaderType;
+        public override Type FeederType => _feederType;
+        public override IFeeder Feeder => _feeder;
+        public override string ExchangeName => _exchangeName;
+
+        /// <summary>
+        /// Construct my default state.
+        /// </summary>
+        /// <param name="stage">This <see cref="Stage"/> of actors I create</param>
+        /// <param name="exchangeName">The name of the exchange I feed</param>
+        /// <param name="feederType">The type of the <see cref="IFeeder"/></param>
+        /// <param name="entryReaderType">The <see cref="IEntryReader{T}"/> used by the <see cref="IFeeder"/></param>
+        private DefaultFeed(Stage stage, string exchangeName, Type feederType, IEntryReader<TextEntry> entryReaderType)
+        {
+            _exchangeName = exchangeName;
+            _feederType = feederType;
+            _entryReaderType = entryReaderType;
+            _feeder = stage.ActorFor<IFeeder>(feederType, this, entryReaderType);
+        }
+        
+        public override Feed<TextEntry> DefaultFeedWith(Stage stage, string exchangeName, Type feederType, IEntryReader<TextEntry> entryReaderType) =>
+            new DefaultFeed(stage, exchangeName, feederType, entryReaderType);
+    }
+}
---
+++ src/Vlingo.Xoom.Lattice/Lattice/Exchange/Feed/Feed.cs
@@ -0,0 +1,85 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using Vlingo.Xoom.Actors;
+using Vlingo.Xoom.Symbio;
+using Vlingo.Xoom.Symbio.Store;
+
+namespace Vlingo.Xoom.Lattice.Exchange.Feed
+{
+    /// <summary>
+    /// Provides support utilities for <see cref="Feed{T}"/> and related types.
+    /// Every <see cref="Feed{T}"/> has an <code>ExchangeName</code>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class Feed<T>
+    {
+        /// <summary>
+        /// Gets the <see cref="IEntryReader{T}"/> to provide entries to the <see cref="IFeeder"/>.
+        /// </summary>
+        public abstract IEntryReader<T> EntryReaderType { get; }
+
+        /// <summary>
+        /// Gets the type to be used by the <see cref="IFeeder"/>.
+        /// </summary>
+        public abstract Type FeederType { get; }
+
+        /// <summary>
+        /// Gets the <see cref="IFeeder"/> per <code>EntryReaderType</code> and <code>FeederType</code>.
+        /// </summary>
+        public abstract IFeeder Feeder { get; }
+
+        /// <summary>
+        /// Gets the defined number of message per feed. In not defined gets the default number of messages per feed
+        /// </summary>
+        public int MessagesPerFeedItem { get; } = 20;
+
+        /// <summary>
+        /// Gets the exchange name
+        /// </summary>
+        public abstract string ExchangeName { get; }
+
+        /// <summary>
+        /// Gets a new <see cref="Feed{T}"/> with the given properties.
+        /// </summary>
+        /// <param name="stage">The <see cref="Stage"/> used to create this feeder</param>
+        /// <param name="exchangeName">The name of this exchange</param>
+        /// <param name="feederType">The <see cref="Actor"/> type of this feeder</param>
+        /// <param name="entryReaderType">The <see cref="IEntryReader{T}"/> that this feeder uses</param>
+        /// <returns><see cref="Feed{T}"/></returns>
+        public abstract Feed<T> DefaultFeedWith(Stage stage, string exchangeName, Type feederType, IEntryReader<T> entryReaderType);
+
+        /// <summary>
+        /// Gets the encoded identity for the <see cref="FeedItemId"/>.
+        /// </summary>
+        /// <param name="feedItemId">The value of the <see cref="FeedItem"/> identity</param>
+        /// <returns><see cref="FeedItemId"/></returns>
+        public virtual FeedItemId ItemId(long feedItemId) => ItemId(feedItemId.ToString());
+        
+        /// <summary>
+        /// Gets the encoded identity for the <see cref="FeedItemId"/>.
+        /// </summary>
+        /// <param name="feedItemId">The value of the <see cref="FeedItem"/> identity</param>
+        /// <returns><see cref="FeedItemId"/></returns>
+        public virtual FeedItemId ItemId(string feedItemId) => new FeedItemId(feedItemId);
+
+        /// <summary>
+        /// Gets the type name for the message associated with the <see cref="IEntry"/>.
+        /// </summary>
+        /// <param name="entry">the <see cref="IEntry"/> used to determine the type name</param>
+        /// <returns>The name of the message type</returns>
+        public virtual string MessageTypeNameFrom(IEntry entry) => entry.TypeName;
+
+        /// <summary>
+        /// Gets the type name for the message associated with the <see cref="IEntry"/>.
+        /// </summary>
+        /// <param name="source">the <see cref="Source{T}"/> used to determine the type name</param>
+        /// <returns>The name of the message type</returns>
+        public virtual string MessageTypeNameFrom(Source<T> source) => source.TypeName;
+    }
+}
