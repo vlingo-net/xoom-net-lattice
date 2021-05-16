f68099d9770d4ad3769958cfc7553825d25f977b
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-05-16 16:03:15 +0200

    Define IFeeder

---
+++ src/Vlingo.Xoom.Lattice/Lattice/Exchange/Feed/IFeeder.cs
@@ -0,0 +1,22 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+namespace Vlingo.Xoom.Lattice.Exchange.Feed
+{
+    /// <summary>
+    /// I provide <see cref="FeedItem"/> to a given <see cref="IFeedConsumer"/>.
+    /// </summary>
+    public interface IFeeder
+    {
+        /// <summary>
+        /// Sends the <see cref="FeedItem"/> identified by <see cref="FeedItemId"/> to <see cref="IFeedConsumer"/>.
+        /// </summary>
+        /// <param name="fromFeedItemId">The <see cref="FeedItemId"/> of the <see cref="FeedItem"/> to send</param>
+        /// <param name="feedConsumer">The <see cref="IFeedConsumer"/> to which the requested <see cref="FeedItem"/> is sent</param>
+        void FeedItemTo(FeedItemId fromFeedItemId, IFeedConsumer feedConsumer);
+    }
+}
