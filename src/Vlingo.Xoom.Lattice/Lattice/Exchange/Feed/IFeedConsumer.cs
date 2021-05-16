f11fa5ed9c01cc8532568d799f7139cab55b42c9
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-05-16 16:01:29 +0200

    Define IFeedConsumer

---
+++ src/Vlingo.Xoom.Lattice/Lattice/Exchange/Feed/IFeedConsumer.cs
@@ -0,0 +1,21 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+namespace Vlingo.Xoom.Lattice.Exchange.Feed
+{
+    /// <summary>
+    /// The protocol used by parties interested in consuming a <see cref="FeedItem"/> stream provided by a <see cref="IFeeder"/>.
+    /// </summary>
+    public interface IFeedConsumer
+    {
+        /// <summary>
+        /// Consumes the <see cref="FeedItem"/> requested of the <code>IFeeder.FeedItemTo(FeedItemId, FeedConsumer)</code>.
+        /// </summary>
+        /// <param name="feedItem">The <see cref="FeedItem"/> requested of the <see cref="IFeeder"/> to be consumed</param>
+        void ConsumeFeedItem(FeedItem feedItem);
+    }
+}
