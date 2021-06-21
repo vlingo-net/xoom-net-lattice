f66b165ea5d1cc05b99590a6b7ef01dcea2d15e0
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-06-21 22:07:26 +0200

    Define TestQueriesActor

--- src/Vlingo.Xoom.Lattice.Tests/Lattice/Query/Fixtures/Store/TestQueriesActor.cs
+++ src/Vlingo.Xoom.Lattice.Tests/Lattice/Query/Fixtures/Store/TestQueriesActor.cs
@@ -5,10 +5,33 @@
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
 
+using Vlingo.Xoom.Common;
+using Vlingo.Xoom.Lattice.Query;
+using Vlingo.Xoom.Symbio;
+using Vlingo.Xoom.Symbio.Store.State;
+
 namespace Vlingo.Tests.Lattice.Query.Fixtures.Store
 {
-    public class TestQueriesActor
+    public class TestQueriesActor : StateStoreQueryActor<TestState>, ITestQueries
     {
-        
+        public TestQueriesActor(IStateStore stateStore) : base(stateStore)
+        {
+        }
+
+        public ICompletes<TestState> TestStateById(string id) => QueryStateFor(id);
+
+        public ICompletes<TestState> TestStateById(string id, TestState notFoundState) => QueryStateFor(id, notFoundState);
+
+        public ICompletes<TestState> TestStateById(string id, int retryInterval, int retryCount) => QueryStateFor(id, retryInterval, retryCount);
+
+        public ICompletes<TestState> TestStateById(string id, TestState notFoundState, int retryInterval, int retryCount) => QueryStateFor(id, notFoundState, retryInterval, retryCount);
+
+        public ICompletes<ObjectState<TestState>> TestObjectStateById(string id) => QueryObjectStateFor(id);
+
+        public ICompletes<ObjectState<TestState>> TestObjectStateById(string id, ObjectState<TestState> notFoundState) => QueryObjectStateFor(id, notFoundState);
+
+        public ICompletes<ObjectState<TestState>> TestObjectStateById(string id, int retryInterval, int retryCount) => QueryObjectStateFor(id, retryInterval, retryCount);
+
+        public ICompletes<ObjectState<TestState>> TestObjectStateById(string id, ObjectState<TestState> notFoundState, int retryInterval, int retryCount) => QueryObjectStateFor(id, notFoundState, retryInterval, retryCount);
     }
 }
