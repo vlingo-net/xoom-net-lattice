6740946330a83a36c966ab7723fc94bcc0a96bdd
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-08-29 13:54:59 +0200

    Pass Await in ItStreamsEmptyStore to 15s for CI

--- src/Vlingo.Xoom.Lattice.Tests/Lattice/Query/StateStoreQueryActorTest.cs
+++ src/Vlingo.Xoom.Lattice.Tests/Lattice/Query/StateStoreQueryActorTest.cs
@@ -131,7 +131,7 @@ namespace Vlingo.Xoom.Lattice.Tests.Lattice.Query
         public void ItStreamsEmptyStore()
         {
             var allStates = new List<TestState>();
-            var testStates = _queries.All(allStates).Await(TimeSpan.FromMilliseconds(10000));
+            var testStates = _queries.All(allStates).Await(TimeSpan.FromSeconds(15));
         
             Assert.Empty(allStates);
             Assert.Empty(testStates);
