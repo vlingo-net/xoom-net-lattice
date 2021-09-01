b04564963432c5fc32d2224c327396a93458297b
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-09-01 21:47:58 +0200

    Define OutBuffers

--- src/Vlingo.Xoom.Lattice.Tests/Util/OutBuffers.cs
+++ src/Vlingo.Xoom.Lattice.Tests/Util/OutBuffers.cs
@@ -5,10 +5,46 @@
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
 
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using Vlingo.Xoom.Lattice.Util;
+using Vlingo.Xoom.Wire.Nodes;
+
 namespace Vlingo.Xoom.Lattice.Tests.Util
 {
     public class OutBuffers
     {
+        private static readonly WeakQueue<Thread> Empty = new WeakQueue<Thread>();
         
+        private readonly Func<WeakQueue<Thread>> _queueInitializer;
+        private readonly ConcurrentDictionary<Id, WeakQueue<Thread>> _buffers;
+        
+        private readonly IHardRefHolder _holder;
+
+        public OutBuffers(IHardRefHolder holder) : this(() => new WeakQueue<Thread>(), holder)
+        {
+        }
+
+        public OutBuffers(Func<WeakQueue<Thread>> queueInitializer, IHardRefHolder holder, int size = 5, int concurrencyLevel = 4)
+        {
+            _queueInitializer = queueInitializer;
+            _holder = holder;
+            _buffers = new ConcurrentDictionary<Id, WeakQueue<Thread>>(concurrencyLevel, size);
+        }
+
+
+        public void Enqueue(Id id, Thread task)
+        {
+            if (!_buffers.ContainsKey(id))
+            {
+                _buffers.AddOrUpdate(id, valueId => _queueInitializer(), (updateId, queue) => _queueInitializer());
+            }
+
+            _holder?.HoldOnTo(task);
+            _buffers.GetOrAdd(id, _queueInitializer()).Enqueue(task);
+        }
+
+        public WeakQueue<Thread> Queue(Id id) => _buffers.GetOrAdd(id, Empty);
     }
 }
