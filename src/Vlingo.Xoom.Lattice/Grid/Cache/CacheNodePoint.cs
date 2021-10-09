9d3a75f15750f858ed28c54ff64defff51da097e
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-10-09 11:21:36 +0200

    Implement CacheNodePoint

---
+++ src/Vlingo.Xoom.Lattice/Grid/Cache/CacheNodePoint.cs
@@ -0,0 +1,26 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using Vlingo.Xoom.Lattice.Grid.Hashring;
+
+namespace Vlingo.Xoom.Lattice.Grid.Cache
+{
+    public class CacheNodePoint<T> : HashedNodePoint<T>
+    {
+        public Cache Cache { get; }
+
+        public CacheNodePoint(Cache cache, int hash, T node) : base(hash, node) => Cache = cache;
+
+        public override void Excluded()
+        {
+        }
+
+        public override void Included()
+        {
+        }
+    }
+}
