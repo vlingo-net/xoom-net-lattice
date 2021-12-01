88b79f5dbf3bfe2965cf94c5887773c05a101d8a
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-12-01 22:36:52 +0100

    Implement IGridRuntime

--- src/Vlingo.Xoom.Lattice/Grid/IGridRuntime.cs
+++ src/Vlingo.Xoom.Lattice/Grid/IGridRuntime.cs
@@ -6,11 +6,25 @@
 // one at https://mozilla.org/MPL/2.0/.
 
 using Vlingo.Xoom.Actors;
+using Vlingo.Xoom.Common.Compiler;
+using Vlingo.Xoom.Lattice.Grid.Application;
+using Vlingo.Xoom.Lattice.Grid.Hashring;
+using Vlingo.Xoom.Wire.Nodes;
 
 namespace Vlingo.Xoom.Lattice.Grid
 {
-    public interface IGridRuntime
+    public interface IGridRuntime : IQuorumObserver
     {
         Actor ActorAt(IAddress address);
+        void RelocateActors();
+        Stage AsStage();
+        void NodeJoined(Id newNode);
+        void SetNodeId(Id nodeId);
+        void SetOutbound(IOutbound outbound);
+        World World { get; }
+        GridNodeBootstrap GridNodeBootstrap { get; }
+        IHashRing<Id> HashRing { get; }
+        IQuorumObserver QuorumObserver { get; }
+        DynaClassLoader WorldClassLoader { get; }
     }
 }
