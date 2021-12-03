d27734c2e4761214a97d7a5017d198fad048aa3e
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-12-03 21:51:58 +0100

    Implement IScheduledScanner

---
+++ src/Vlingo.Xoom.Lattice/Grid/Spaces/IScheduledScanner.cs
@@ -0,0 +1,20 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+namespace Vlingo.Xoom.Lattice.Grid.Spaces
+{
+    public interface IScheduledScanner<T>
+    {
+        void Scan();
+        void ScheduleBy(IScheduledScannable<T> scannable);
+    }
+
+    public interface IScheduledScannable<T>
+    {
+        T Scannable();
+    }
+}
