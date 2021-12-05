36f1f72a9398d1cc13365eef556a403d6654421d
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-12-05 18:09:36 +0100

    Implement PartitioningSpaceRouter

--- src/Vlingo.Xoom.Lattice/Grid/Spaces/PartitioningSpaceRouter.cs
+++ src/Vlingo.Xoom.Lattice/Grid/Spaces/PartitioningSpaceRouter.cs
@@ -13,50 +13,58 @@ namespace Vlingo.Xoom.Lattice.Grid.Spaces
 {
     public class PartitioningSpaceRouter : Actor, ISpace
     {
-        private ISpace[] partitions;
-        private int totalPartitions;
+        private readonly ISpace[] _partitions;
+        private readonly int _totalPartitions;
 
         public PartitioningSpaceRouter(int totalPartitions, TimeSpan defaultScanInterval)
         {
-            this.totalPartitions = totalPartitions;
-            this.partitions = new ISpace[totalPartitions];
+            _totalPartitions = totalPartitions;
+            _partitions = new ISpace[totalPartitions];
 
             Initialize(defaultScanInterval);
         }
         
         public ICompletes<T> ItemFor<T>(Type actorType, params object[] parameters)
         {
-            throw new NotImplementedException();
+            // Fail; not implemented. See SpaceItemFactoryRelay#itemFor.
+            return Completes().With<T>(default!);
         }
 
         public ICompletes<KeyItem<T>> Put<T>(IKey key, Item<T> item)
         {
-            throw new NotImplementedException();
+            var completes = CompletesEventually();
+            SpaceOf(key).Put(key, item).AndThenConsume(keyItem => completes.With(keyItem));
+            return Completes<KeyItem<T>>();
         }
 
         public ICompletes<Optional<KeyItem<T>>> Get<T>(IKey key, Period until)
         {
-            throw new NotImplementedException();
+            var completes = CompletesEventually();
+            SpaceOf(key).Get<T>(key, until).AndThenConsume(keyItem => completes.With(keyItem));
+            return Completes<Optional<KeyItem<T>>>();
         }
 
         public ICompletes<Optional<KeyItem<T>>> Take<T>(IKey key, Period until)
         {
-            throw new NotImplementedException();
+            var completes = CompletesEventually();
+            SpaceOf(key).Take<T>(key, until).AndThenConsume(keyItem => completes.With(keyItem));
+            return Completes<Optional<KeyItem<T>>>();
         }
         
         private void Initialize(TimeSpan defaultScanInterval)
         {
-            for (var count = 0; count < totalPartitions; ++count)
+            for (var count = 0; count < _totalPartitions; ++count)
             {
-                //var definition = Definition.Has(SpaceActor.class, new SpaceInstantiator(defaultScanInterval), address().name() + "-" + count);
-                //final Space internalSpace = childActorFor(Space.class, definition);
-                //partitions[count] = internalSpace;
+                var definition = Definition.Has(() => new SpaceActor(defaultScanInterval), $"{Address.Name}-{count}");
+                var internalSpace = ChildActorFor<ISpace>(definition);
+                _partitions[count] = internalSpace;
             }
         }
 
-        // private Space spaceOf(final Key key) {
-        //     final int partition = key.hashCode() % totalPartitions;
-        //     return partitions[partition];
-        // }
+        private ISpace SpaceOf(IKey key)
+        {
+            var partition = key.GetHashCode() % _totalPartitions;
+            return _partitions[partition];
+        }
     }
 }
