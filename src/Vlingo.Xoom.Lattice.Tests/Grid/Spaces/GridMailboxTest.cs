ed2faac09c006a1ab194bff132b0d34ded996eca
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-12-14 23:14:42 +0100

    Implement GridMailboxTest

---
+++ src/Vlingo.Xoom.Lattice.Tests/Grid/Spaces/GridMailboxTest.cs
@@ -0,0 +1,68 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using System.Threading;
+using Vlingo.Xoom.Actors.Plugin.Logging.Console;
+using Vlingo.Xoom.Actors.Plugin.Mailbox.TestKit;
+using Vlingo.Xoom.Lattice.Grid;
+using Vlingo.Xoom.Lattice.Grid.Cache;
+using Vlingo.Xoom.Lattice.Grid.Hashring;
+using Vlingo.Xoom.Wire.Nodes;
+using Xunit;
+
+namespace Vlingo.Xoom.Lattice.Tests.Grid.Spaces
+{
+    public class GridMailboxTest
+    {
+        private static Cache cache = Cache.DefaultCache();
+        private Func<int, Id, HashedNodePoint<Id>> factory =  (hash, node) => new CacheNodePoint<Id>(cache, hash, node);
+        
+        [Fact]
+        public void TestMurmurArrayHashRing()
+        {
+            var hashRing = new MurmurArrayHashRing<Id>(100, factory);
+
+            var localId = Id.Of(1);
+            var localMailbox = new TestMailbox();
+            var address = new GridAddress(Guid.NewGuid());
+
+            hashRing.IncludeNode(localId);
+            hashRing.IncludeNode(Id.Of(2));
+            hashRing.IncludeNode(Id.Of(3));
+            hashRing.IncludeNode(Id.Of(4));
+
+            var gridMailbox = new GridMailbox(localMailbox, localId, address, hashRing, null, ConsoleLogger.TestInstance());
+
+            Assert.False(gridMailbox.IsClosed);
+            gridMailbox.Close();
+            Assert.True(gridMailbox.IsClosed);
+            Assert.True(gridMailbox.ConcurrencyCapacity > 0);
+        }
+
+        [Fact]
+        public void TestMD5ArrayHashRing()
+        {
+            var hashRing = new MD5ArrayHashRing<Id>(100, factory);
+
+            var localId = Id.Of(1);
+            var localMailbox = new TestMailbox();
+            var address = new GridAddress(Guid.NewGuid());
+
+            hashRing.IncludeNode(localId);
+            hashRing.IncludeNode(Id.Of(2));
+
+            var gridMailbox = new GridMailbox(localMailbox, localId, address, hashRing, null, ConsoleLogger.TestInstance());
+
+            Assert.False(gridMailbox.IsClosed);
+            gridMailbox.Close();
+            Thread.Sleep(10);
+            Assert.True(gridMailbox.IsClosed);
+            Assert.True(gridMailbox.ConcurrencyCapacity > 0);
+        }
+    }
+}
